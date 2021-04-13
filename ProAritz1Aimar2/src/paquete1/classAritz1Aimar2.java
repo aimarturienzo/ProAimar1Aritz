54f48576178fad07d0ea264e39e31dd7d73be139
aimarturienzo <82277755+aimarturienzo@users.noreply.github.com>
2021-04-13 10:24:11 +0200

    Add files via upload

---
+++ ProAritz1Aimar2/src/paquete1/classAritz1Aimar2.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class classAritz1Aimar2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo!");
+		System.out.println("Hola de nuevo mundo!");
+	}
+
+}
