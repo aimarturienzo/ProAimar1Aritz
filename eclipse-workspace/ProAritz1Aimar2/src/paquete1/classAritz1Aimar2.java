776fed65655c348746d74d829376c753debbd61c
aimar <aimar@LAPTOP-R6V2O4JB>
2021-04-13 10:33:54 +0200

    cambio1

---
+++ eclipse-workspace/ProAritz1Aimar2/src/paquete1/classAritz1Aimar2.java
@@ -0,0 +1,12 @@
+package paquete1;
+
+public class classAritz1Aimar2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo!");
+		System.out.println("Hola de nuevo Aritz!");
+		System.out.println("Hasta luego Aritz");
+	}
+
+}
