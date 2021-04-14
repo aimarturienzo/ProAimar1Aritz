1e246bf4268c6dfcd58e887869c0750da3d6cd1d
aimarturienzo <aimarturienzo@gmail.com>
2021-04-14 11:47:56 +0200

    cambio2Aimar

---
+++ eclipse-workspace/proAimar1Aritz2/src/proAimar1Aritz2/classAimar1Aritz2.java
@@ -0,0 +1,10 @@
+package proAimar1Aritz2;
+
+public class classAimar1Aritz2 {
+	public static void main(String[]args) {
+		System.out.println("Hola mundo!");
+		System.out.println("Adios mundo!");
+		System.out.println("Hola de nuevo mundo!");
+		System.out.println("Adios de nuevo mundo!");
+	}
+}
